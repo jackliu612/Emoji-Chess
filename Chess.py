2a23107f6c74c94e9204a156f85eaacb47f4481b
Jack <jackliu612@gmail.com>
2019-12-27 22:12:58 -0500

    Added dummy methods to chess class and created a python notebook for testing.
    Also updated gitignore for checkpoints and other python extraneous files

--- Chess.py
+++ Chess.py
@@ -1,7 +1,13 @@
 # Chess class for logic etc
 # Author: Jack Liu
+# 12/27/2019
 
 
 class Chess:
+    test = 'hi'
+
     def __init__(self):
-        turn = 0
+        self.turn = 0
+
+    def to_string(self):
+        return ""
